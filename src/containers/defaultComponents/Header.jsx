1c82f78f334604c3437ef589ad6d13a082e4a930
Sayat <sayatumarov1@gmail.com>
2021-08-16 16:35:15 +0600

    header

--- src/containers/defaultComponents/Header.jsx
+++ src/containers/defaultComponents/Header.jsx
@@ -2,16 +2,22 @@ import React from 'react';
 import {NavLink} from "react-router-dom";
 import s from "./Header.module.css";
 import netshopLogo from "../../images/netshopLogo.png";
+import ava from "../../images/ava.jpeg";
 
 export default function Header(props) {
     return(
-        <div className={s.body}>
-            <div className={s.navbar}>
-            <div className={s.logo}><img src={netshopLogo}></img></div>
-                <h2>NETSHOP</h2>
-                { props.isAuth ? props.login
-                : <NavLink to={'/login'}>Login</NavLink> }
+        <div className={s.header}>
+                <img className={s.logo} src={netshopLogo}></img>
+                <NavLink className={s.nav} to='/'>КАТЕГОРИИ</NavLink>
+                <NavLink className={s.nav} to='/'>
+                    <span>В ТРЕНДЕ</span>
+                </NavLink>
+                <NavLink className={s.nav} to='/'>ИЗБРАННОЕ</NavLink>
+                <div className={s.user}>
+                    <img src={ava} alt="ava" />
+                    { props.isAuth ? props.login
+                : <NavLink to='/login' >Login</NavLink> }
+                </div>
             </div>
-        </div>
     )
 }
