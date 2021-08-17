1b5384fe0c836f14657ffbc0a798247081b49fbd
Sayat <sayatumarov1@gmail.com>
2021-08-17 13:35:33 +0600

    fixing bugs

--- src/containers/defaultComponents/Header.jsx
+++ src/containers/defaultComponents/Header.jsx
@@ -7,7 +7,6 @@ import bck from "./background.png"
 export default function Header(props) {
     return(
         <div className={s.header}>
-                <img className={s.bck} src={bck} alt="background" />
                 <img className={s.logo} src={netshopLogo}></img>
                 <NavLink className={s.nav} to='/'>КАТЕГОРИИ</NavLink>
                 <NavLink className={s.nav} to='/'>
@@ -19,6 +18,7 @@ export default function Header(props) {
                     { props.isAuth ? props.login
                 : <NavLink to='/login' >Login</NavLink> }
                 </div>
+                <img className={s.bck} src={bck} alt="background" />
             </div>
     )
 }
